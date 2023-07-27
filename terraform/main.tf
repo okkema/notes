9919e5f652dd7a9364ada4041caad5f22e1b54b5
Benjamin Okkema <ben@okkema.org>
2023-07-26 22:09:58 -0500

    feat: github group

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,7 @@ locals {
     "TF_API_TOKEN" : var.TF_API_TOKEN,
     "ACTIONS_GITHUB_TOKEN" : var.ACTIONS_GITHUB_TOKEN
   }
+  name = "notes"
 }
 
 module "secrets" {
@@ -23,20 +24,38 @@ module "bucket" {
   account_id = var.cloudflare_account_id
   access_key = var.cloudflare_r2_access_key
   secret_key = var.cloudflare_r2_secret_key
-  bucket     = "notes"
+  bucket     = local.name
 }
 
 module "worker" {
-  source  = "app.terraform.io/okkema/worker/cloudflare"
-  version = "0.3.0"
+  source     = "app.terraform.io/okkema/worker/cloudflare"
+  version    = "0.4.0"
+  depends_on = [module.bucket]
 
   account_id = var.cloudflare_account_id
   zone_id    = var.cloudflare_zone_id
-  name       = "notes"
+  name       = local.name
   content    = file(abspath("${path.module}/../dist/index.js"))
-  hostnames  = ["public.notes.${var.cloudflare_zone}", "private.notes.${var.cloudflare_zone}"]
+  hostnames  = ["public.${local.name}", "private.${local.name}"]
   buckets = [{
     binding = "NOTES"
-    name    = "notes"
+    name    = local.name
   }]
+}
+
+module "team" {
+  source  = "app.terraform.io/okkema/team/github"
+  version = "0.1.0"
+
+  name = local.name
+}
+
+module "application" {
+  source     = "app.terraform.io/okkema/application/cloudflare"
+  version    = "0.2.2"
+  depends_on = [module.team, module.worker]
+
+  zone_id      = var.cloudflare_zone_id
+  name         = "private.${local.name}"
+  github_teams = [local.name]
 }
