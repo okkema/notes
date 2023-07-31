eab1075f708f2305f4b09e9adcab0ba13ae34d7f
Benjamin Okkema <ben@okkema.org>
2023-07-30 21:19:33 -0500

    refactor: move functions

--- functions/index.test.ts
+++ functions/index.test.ts
@@ -1,22 +1,27 @@
 import { unstable_dev } from "wrangler"
 import type { UnstableDevWorker } from "wrangler"
 import { describe, expect, it, beforeAll, afterAll } from "vitest"
+import { Miniflare } from "miniflare"
 
 describe("Worker", () => {
 	let worker: UnstableDevWorker
 
 	beforeAll(async () => {
-		worker = await unstable_dev("src/index.ts", { experimental: { disableExperimentalWarning: true } })
+		worker = await unstable_dev("functions/index.ts", {
+			experimental: { 
+				disableExperimentalWarning: true 
+			} 
+		})
 	})
 
 	afterAll(async () => {
 		await worker.stop()
 	})
 
-	it("should return 200 response", async () => {
+	it("should return 404 response", async () => {
 		const req = new Request("https://example.com", { method: "GET" })
 		const resp = await worker.fetch(req.url)
-		expect(resp.status).toBe(200)
+		expect(resp.status).toBe(404)
 
 		const text = await resp.text()
 		expect(text).toBeDefined()
